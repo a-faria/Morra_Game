d397d530c52eff72f21a9d18a9e84ad354b044d2
a-faria <122120022+a-faria@users.noreply.github.com>
2023-03-30 11:09:49 +0100

    Create morra_game_V1.py

---
+++ morra_game_V1.py
@@ -0,0 +1,9 @@
+import math
+import random
+p_choice = int(input('which player do you want to be? type in even or odds?:\n'))
+if p_choice.lower=="odds":
+    #run odds function
+    elif p_choice.lower=="even":
+         #run even function
+    else:
+        print("error you have to type "E" or "O" ")
